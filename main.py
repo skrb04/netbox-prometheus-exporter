4249e1737ae7f8ba437a2e014ccdf2c03dab4103
skrb04 <99761675+skrb04@users.noreply.github.com>
2022-07-09 12:01:48 -0700

    Update main.py

--- main.py
+++ main.py
@@ -4,7 +4,6 @@ import re
 from prometheus_client import start_http_server, Metric, REGISTRY
 from prometheus_client.metrics_core import InfoMetricFamily
 from prometheus_client.core import CounterMetricFamily
-from prometheus_client.core import GaugeMetricFamily
 
 from environs import Env
 
@@ -34,8 +33,9 @@ class NetboxCollector(object):
         # This is unique to our netbox env, you may need to customize these
         device_type = netbox.dcim.devices.all()
         labels = ['site_group', 'manufacturer', 'name', 'tenant']
-        metric2 = GaugeMetricFamily("netbox_device", "Netbox Device", labels=labels)
+        metric2 = InfoMetricFamily("netbox_device", "Netbox Device", labels=labels)
         for device in device_type:
+            # regex substitution for the site_group
             site_group = re.sub('^(.*)/.*$', r'\1', device.site.name)
             manufacturer = device.device_type.manufacturer
             name = device.name
